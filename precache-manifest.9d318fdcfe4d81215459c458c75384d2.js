42ded7749a18a278a67ac9f34a3bcbb7637c5024
holgergp <holgergp@gmail.com>
2021-02-05 17:07:52 +0000

    Deploying to gh-pages from  @ 6998397a79800f22c995568e18aaec981fc700db 🚀

--- precache-manifest.9d318fdcfe4d81215459c458c75384d2.js
+++ precache-manifest.9d318fdcfe4d81215459c458c75384d2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68fd3200ae3c95523c35ce62f0bc9917",
+    "revision": "67f2b89887195710c39bef576758244a",
     "url": "/soccerReact16/index.html"
   },
   {
-    "revision": "7f5f760f22ead74ecd19",
+    "revision": "43104ef5a046af387ce3",
     "url": "/soccerReact16/static/css/2.2df6b46e.chunk.css"
   },
   {
@@ -12,12 +12,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/soccerReact16/static/css/main.42401034.chunk.css"
   },
   {
-    "revision": "7f5f760f22ead74ecd19",
-    "url": "/soccerReact16/static/js/2.e465ccdf.chunk.js"
+    "revision": "43104ef5a046af387ce3",
+    "url": "/soccerReact16/static/js/2.38f10f31.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/soccerReact16/static/js/2.e465ccdf.chunk.js.LICENSE.txt"
+    "url": "/soccerReact16/static/js/2.38f10f31.chunk.js.LICENSE.txt"
   },
   {
     "revision": "98766b3629c5dac788e4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/soccerReact16/precache-manifest.010454f493b73a22e1d276fd69361f81.js"
+  "/soccerReact16/precache-manifest.9d318fdcfe4d81215459c458c75384d2.js"
 );
 
 self.addEventListener('message', (event) => {
